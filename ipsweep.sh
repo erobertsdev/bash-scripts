a4c6c3db23636277cd4bdac8f775ded01200b171
Elias Roberts <elias@eroberts.dev>
2023-02-11 14:12:17 -0700

    added if statement for empty ip

--- ipsweep.sh
+++ ipsweep.sh
@@ -7,6 +7,12 @@ for ip in 192.168.220.{1..254}; do
 done
 
 # Alternate version
-#for ip in `seq 1 254`; do
-#  ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
-#done
+if [ "$1" == "" ]
+then
+echo "You forgot an IP address!"
+echo "Syntax: ./ipsweep.sh 192.168.1"
+else
+for ip in `seq 1 254`; do
+ ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
+done
+fi
