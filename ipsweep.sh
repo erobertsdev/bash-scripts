3a8e7b53380b66a93e681f0fbe0cbd53a14cd8ae
Elias Roberts <elias@eroberts.dev>
2023-02-11 14:09:47 -0700

    a crappy ipsweeper

---
+++ ipsweep.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script will ping a range of IP addresses and report which ones are up.
+# It will also report the hostname of the machine if it is up.
+for ip in 192.168.220.{1..254}; do
+  ping -c 1 $ip | grep "64 bytes" | cut -d " " -f 4 | sed 's/.$//'
+done
+
+# Alternate version
+#for ip in `seq 1 254`; do
+#  ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
+#done
